9339586de2eba245e60daf5bf43dbac660ac519b
BuildTools <unconfigured@null.spigotmc.org>
2020-04-17 10:20:15 +0800

    新增脚本让网页无法被其他网页嵌套

---
+++ js/necessary.js
@@ -0,0 +1,10 @@
+/**
+ * @author       chenzhongyi <chenzhongyi.net>
+ * @copyright    2020 Zhongyi Chen.
+ */
+
+(function() {
+  if (top !== window) {
+    top.location.href = window.location.href;
+  }
+}());
